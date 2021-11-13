643b2e2d6a1e2b23f7aa6983769caf7567736c83
ArtemKhairov <artemxairoff@yandex.ru>
2021-11-13 13:02:12 +0700

    add methods for line width

--- src/components/SettingBar.js
+++ src/components/SettingBar.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import '../styles/toolbar.scss'
+import React from "react";
+import "../styles/toolbar.scss";
+import toolState from "../store/toolState"
 
 const SettingBar = () => {
   return (
-    <div className="toolbar" style={{top:"40px"}}>
-      
+    <div className="toolbar" style={{ top: "40px" }}>
+      <label htmlFor="line-width"> Толщина линии</label>
+      <input
+        style={{ margin: "0 10px" }}
+        id="line-width"
+        type="number"
+        min={1}
+        max={50}
+        defaultValue={1}
+        onChange={(e)=>toolState.setLineWidth(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SettingBar
+export default SettingBar;
--- src/tools/Tool.js
+++ src/tools/Tool.js
@@ -7,6 +7,21 @@ export default class Tool {
     this.ctx = canvas.getContext("2d");
   }
 
+  // сеттер цвета
+  set fillColor(color) {
+    this.ctx.fillStyle = color;
+  }
+
+  // сеттер цвета обводки
+  set strokeColor(color) {
+    this.ctx.stokeStyle = color;
+  }
+
+  // сеттер ширины линии
+  set lineWidth(width) {
+    this.ctx.lineWidth = width;
+  }
+
   destroyEvents() {
     this.canvas.onmousemove = null;
     this.canvas.onmousedown = null;
