83e86186e7852a637c72606ae9d315774ee9eaa0
ArtemKhairov <artemxairoff@yandex.ru>
2021-11-15 20:21:03 +0700

    add JSON parse and stringify,switch msg.method

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const app = express();
+// Инициализация роутов по веб-сокету
 const WSServer = require("express-ws")(app);
-
+// Широковещательная рассылка
+const aWss = WSServer.getWss();
 const PORT = process.env.PORT || 5000;
 
 app.ws("/", (ws, req) => {
   console.log("Connected app.ws");
   ws.send("***Ты успешно подключился message by Server ***");
   ws.on("message", (msg) => {
-    console.log(msg);
+    switch (msg.method) {
+      case "connection":
+        break;
+      case "message":
+        break;
+    }
   });
 });
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+
+const connectionHandler = (ws, msg) => {
+  msg = JSON.parse(msg);
+  ws.id = msg.id;
+};
+
+const broadcastConnection = (ws, msg) => {};
--- testws/index.js
+++ testws/index.js
@@ -2,14 +2,28 @@ let btn = document.getElementById("3070");
 const socket = new WebSocket("ws://localhost:5000");
 
 socket.onopen = () => {
-  console.log("Соединение с сервером установлено");
+  // console.log("Соединение с сервером установлено");
+  socket.send(
+    JSON.stringify({
+      method: "connection",
+      id: "1",
+      name: "Artem",
+    })
+  );
 };
 
 socket.onmessage = (event) => {
   console.log("We have new message CAP", event.data);
 };
 
-
-btn.onclick=(e)=>{
-  socket.send('Hello server *by user*')
-}
+btn.onclick = (e) => {
+  // socket.send('Hello server *by user*')
+  socket.send(
+    JSON.stringify({
+      method: "message",
+      message: "Give me 2080 Ti",
+      name: "Artem",
+      id: "1",
+    })
+  );
+};
