5d2d1afcdeca82fc6f5b3b1f9a4f7c8bf5b68150
ArtemKhairov <artemxairoff@yandex.ru>
2021-11-15 23:18:31 +0700

    react-router broken

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,34 @@ import Canvas from "./components/Canvas";
 import SettingBar from "./components/SettingBar";
 import Toolbar from "./components/Toolbar";
 import "./styles/app.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Routes as Switch } from "react-router-dom";
+import { Navigate as Redirect } from "react-router-dom";
+import React, { Fragment } from "react";
 
 function App() {
   return (
-    <div className="app">
-      <Toolbar />
-      <SettingBar />
-      <Canvas />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route
+            path="/f43"
+            render={()=>{
+
+            
+              <>
+                <Toolbar />
+                <SettingBar />
+                <Canvas />
+              </>
+              }
+            }
+          ></Route>
+          {/* <Redirect to={`f${(+new Date()).toString(16)}`} /> */}
+        </Switch>
+        {/* <Redirect to={"4"}/> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ app.ws("/", (ws, req) => {
   console.log("Connected app.ws");
   ws.send("***Ты успешно подключился message by Server ***");
   ws.on("message", (msg) => {
+    msg = JSON.parse(msg);
     switch (msg.method) {
       case "connection":
+        connectionHandler(ws, msg);
         break;
       case "message":
         break;
@@ -22,8 +24,14 @@ app.ws("/", (ws, req) => {
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
 
 const connectionHandler = (ws, msg) => {
-  msg = JSON.parse(msg);
   ws.id = msg.id;
+  broadcastConnection(ws, msg);
 };
 
-const broadcastConnection = (ws, msg) => {};
+const broadcastConnection = (ws, msg) => {
+  aWss.clients.forEach((client) => {
+    if (client.id === msg.id) {
+      client.send(`Пользователь ${msg.name} подключился`);
+    }
+  });
+};
